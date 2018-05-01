78b950ecb2c5f70c0458fa7710290f7e29d71904
Stefan Cross <scross@Stefans-MacBook-Pro.local>
2018-05-01 09:08:34 +0100

    Alteration from tek laptop

---
+++ index.php
@@ -0,0 +1,89 @@
+<?php include 'header.php' ?>
+    <!-- Full Width Image Header with Logo -->
+    <!-- Image backgrounds are set within the full-width-pics.css file. -->
+    <header class="image-bg-fluid-height-main">
+        <img class="img-responsive img-center" src="images/FFlogotype_logo4.png" alt="">
+    </header>
+
+    <!-- Content Section -->
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="section-heading">24-25-26 August 2018</h1>
+                    <a href="tickets.html">
+                        <button type="button" class="btn btn-success">TICKETS - NOW ON SALE!</button>
+                    </a>
+                    <p class="lead section-lead">London Cafe and Custom Presents: <b>Fuelled Moto Film Fest</b>, London’s only independent motorcycle film festival, 24-25-26 August.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Fixed Height Image Aside -->
+    <!-- Image backgrounds are set within the full-width-pics.css file. -->
+    <aside class="image-bg-fixed-height-3"></aside>
+
+    <!-- Content Section -->
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p class="section-paragraph">After a sold out first event in 2017, which featured in Time Out Magazine’s recommended events, we’re back for round 2. With pop up screenings across three cool, intimate, motorcycle-related venues across East London, we’ll be showing a handpicked selection of the best short and feature length motorcycle films around.</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+
+    <!-- Fixed Height Image Aside -->
+    <!-- Image backgrounds are set within the full-width-pics.css file. -->
+    <aside class="image-bg-fixed-height-2"></aside>
+
+    <!-- Content Section -->
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p class="section-paragraph">Last year we welcomed over 200 motorbike and film enthusiasts over the duration of the festival, with a hugely oversubscribed launch night at Dark Arts Coffee in Hackney.</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+
+    <aside class="image-bg-fixed-height-1"></aside>
+
+    <!-- Content Section -->
+    <section>
+        <div class="container">
+            <div class="row">
+                <!--<div class="col-lg-12">-->
+                <!--<h1 class="section-heading">Contact us</h1>-->
+                <!--<p class="lead section-lead">Want to help out?! Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>-->
+                <!--<p class="section-paragraph">Or suggestions welcome... Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, suscipit, rerum quos facilis repellat architecto commodi officia atque nemo facere eum non illo voluptatem quae delectus odit vel itaque amet.</p>-->
+                <!--</div>-->
+                <!--<div class="col-md-offset-2">-->
+                <!--<div class="col-lg-10">-->
+                <!--<form>-->
+                <!--<div class="col-lg-5">-->
+                <!--<input type="text" class="form-control" placeholder="Name">-->
+                <!--</div>-->
+                <!--<div class="col-lg-5">-->
+                <!--<input type="text" class="form-control" placeholder="Email">-->
+                <!--</div>-->
+                <!--<div class="col-lg-10">-->
+                <!--<textarea class="form-control" rows="3" placeholder="Message..."></textarea>-->
+                <!--<button type="submit" class="btn btn-primary">Submit</button>-->
+                <!--</div>-->
+                <!--</form>-->
+                <!--</div>-->
+                <!--</div>-->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+<?php include 'footer.php' ?>
